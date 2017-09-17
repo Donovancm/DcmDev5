b0dbe1119ddf61c5ebd5fee9f54665ca8dd22e86
Donovancm <donovan.dcm@gmail.com>
2017-09-17 21:51:06 +0200

    one

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Model;
+using System.Linq;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //InsertData();
+            ViewData();
+        }
+        static void InsertData()
+        {
+            using (var db = new MovieContext())
+            {
+                Movie m = new Movie
+                {
+                    Title = "No country for old men",
+                    Actors = new System.Collections.Generic.List<Actor> {
+                        new Actor{Name = "Tommy Lee"},
+                        new Actor{Name = "Xavier Berdem"}
+                    }
+                };
+                db.Movies.Add(m);
+                db.SaveChanges();
+            }
+        }
+        static void ViewData()
+        {
+            using (var db = new MovieContext())
+            {
+                foreach (var movie in db.Movies)
+                {
+                    Console.WriteLine("Found movie with title" + movie.Title);
+                    foreach (var actor in db.Actors.Where(a => movie.Id == a.MovieId))
+                    {
+                        Console.WriteLine("Found actor with name" + actor.Name);
+                    }
+                }
+            }
+        }
+    }
+}
